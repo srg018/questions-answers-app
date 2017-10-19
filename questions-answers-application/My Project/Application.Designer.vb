0adea8a9e6bff46d791abb78e530a602b1ac126f
Jeff Puckett <jeff@jeffpuckett.com>
2017-10-19 08:33:38 -0500

    rename welcome form

--- questions-answers-application/My Project/Application.Designer.vb
+++ questions-answers-application/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.questions_answers_application.Form1
+            Me.MainForm = Global.questions_answers_application.welcome
         End Sub
     End Class
 End Namespace
