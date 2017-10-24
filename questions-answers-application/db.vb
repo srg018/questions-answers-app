71a9f66481020f79cda7eaf8af2f55bb82347b87
wxk001 <wxk001@email.uark.edu>
2017-10-24 09:07:40 -0500

    Created data wrapper

---
+++ questions-answers-application/db.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data.SqlClient
+
+Public Class db
+    Dim sqlcon As New SqlConnection With {.ConnectionString = "Server=essql1.walton.uark.edu;Database=isys4283-2017fa;Trusted_Connection=yes;"}
+    Dim sqlcmd As New SqlCommand With {.Connection = sqlcon}
+
+    'Fuction returns value, asks for something
+    'sub routine is just process
+
+    Public Property sql() As String
+        Get
+            Return sqlcmd.CommandText
+        End Get
+
+        Set(value As String)
+            sqlcmd.CommandText = value
+        End Set
+    End Property
+    'ability to set give sql command
+    Public Sub fill(ByRef dgv As DataGridView)
+
+
+        Dim sqlda As SqlDataAdapter
+        Dim sqldataset As DataSet
+
+        Try
+            sqlcon.Open()
+            sqlda = New SqlDataAdapter(sqlcmd)
+            sqldataset = New DataSet
+            sqlda.Fill(sqldataset)
+            If sqldataset.Tables.Count > 0 Then
+                dgv.Refresh()
+                dgv.DataSource = sqldataset.Tables(0)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Throw ex
+        Finally
+            If sqlcon.State = ConnectionState.Open Then
+                sqlcon.Close()
+            End If
+        End Try
+    End Sub
+End Class
