79ef4f7ebd1dee1f297a926ddbcfdfef81f6a183
srg018 <srg018@uark.edu>
2017-10-24 09:07:22 -0500

    Create database class

--- questions-answers-application/welcome.vb
+++ questions-answers-application/welcome.vb
@@ -1,33 +1,8 @@
-﻿Imports System.Data.SqlClient
-
-Public Class welcome
+﻿Public Class welcome
+    Protected db As New db
     Protected Sub LoadQuestions()
-        Dim sqlcon As New SqlConnection With {.ConnectionString = "Server=essql1.walton.uark.edu;Database=isys4283-2017fa;Trusted_Connection=yes;"}
-        Dim sqlcmd As SqlCommand
-        Dim sqlda As SqlDataAdapter
-        Dim sqldataset As DataSet
-
-        Dim query As String
-        query = "SELECT * FROM questions ORDER BY created_at DESC;"
-
-        Try
-            sqlcon.Open()
-            sqlcmd = New SqlCommand(query, sqlcon)
-            sqlda = New SqlDataAdapter(sqlcmd)
-            sqldataset = New DataSet
-            sqlda.Fill(sqldataset)
-            If sqldataset.Tables.Count > 0 Then
-                dgvQuestions.Refresh()
-                dgvQuestions.DataSource = sqldataset.Tables(0)
-            End If
-        Catch ex As Exception
-            MsgBox(ex.Message)
-            Throw ex
-        Finally
-            If sqlcon.State = ConnectionState.Open Then
-                sqlcon.Close()
-            End If
-        End Try
+        db.sql = "SELECT * FROM questions ORDER BY created_at DESC;"
+        db.fill(dgvQuestions)
     End Sub
     Private Sub LoadToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadToolStripMenuItem.Click
         LoadQuestions()
